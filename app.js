88341ddd7fd3d7e22740231315e15d3e2dec9e87
Umar-Khitab <khitab.khan007@gmail.com>
2025-01-20 22:17:31 +0500

    give port default value

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const app = express();
 app.get("/", (req,res) =>{
     res.json("Welcome the node js app")
 })
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => console.log("server is running on port 3000"))
