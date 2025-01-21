d3fe5f147a3442484902eed65af760acf02ffbbe
Umar-Khitab <khitab.khan007@gmail.com>
2025-01-21 22:10:54 +0500

    add home page route

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 app.get("/", (req,res) =>{
     res.json("Welcome the node js app")
 })
+app.get("/home", (req,res) => {
+    res.json("welcome again in home page")
+})
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => console.log("server is running on port 3000"))
